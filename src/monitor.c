f4eb06274b0a0fae8d9c229c6f35c3e7da5b3a8c
rafa2002 <73424228+rafa2002@users.noreply.github.com>
2023-04-13 17:57:56 +0100

    Tenho que rever a string de input para adaptar

--- src/monitor.c
+++ src/monitor.c
@@ -11,5 +11,10 @@
 #include "client.h"
 
 void armazenar_info_processo(int pid, char ** comando, struct tm *timestamp){
-
+    int log = open("log.txt",O_WRONLY | O_CREAT | O_TRUNC,0660);
+    char *buf;
+    char tempo[20];
+    strftime(tempo,sizeof(tempo),"%Y-%m-%d %H:%M:%S",timestamp);
+    printf("Data formatada: %s\n",tempo);
+    write(log,&buf,sizeof(buf));
 }
