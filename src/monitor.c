826eb559dfded7f7c9038932b9598948ec7f28b7
rafa2002 <rafacferreira2002@gmail.com>
2023-04-27 18:52:20 +0100

    Mudei cenas

--- src/monitor.c
+++ src/monitor.c
@@ -6,12 +6,117 @@
 #include <sys/stat.h>
 #include <windows.h>
 #include <io.h>
-// #include <unistd.h>
-// #include <wait.h>
-#include <limits.h>
+//#include <unistd.h>
+#include <string.h>
 #include <time.h>
-#include "tracer.h"
+#include <signal.h>
+#include <stdbool.h>
 
+#define MAX_COMMAND 256
+
+int running = true;
+
+int main(int argc, char **argv) {
+
+    if (mkfifo("canal", 0600) == -1 && errno != EEXIST) {
+        perror("Erro ao criar o canal nomeado");
+        exit(1);
+    }
+
+    int canal = open("canal", O_RDONLY | O_WRONLY, 0600);
+    if (canal == -1) {
+        perror("Erro ao abrir o canal nomeado");
+        exit(1);
+    }
+
+    char buffer[MAX_COMMAND] = "";
+    
+    while (running) {
+        int bytes_lidos = 0;
+        char byte_lido;
+        while(read(canal,&byte_lido, 1)){
+            // Se o byte lido for o caractere nulo, processa a mensagem
+            if (byte_lido == '\0') {
+                printf("Mensagem recebida: %s\n", buffer);
+                // Aqui você pode processar a mensagem como desejar
+                // Reinicializa o buffer para a próxima mensagem
+                //buffer[0] = '\0';
+                strncat(buffer, &byte_lido, 1);
+                break;
+            } else {
+                // Se o byte lido não for o caractere nulo, adiciona ao buffer
+                strncat(buffer, &byte_lido, 1);
+            }
+        }
+        if (bytes_lidos == -1) {
+            perror("Erro ao ler do canal");
+            exit(EXIT_FAILURE);
+        } else if (bytes_lidos == 0) {
+            // Fim do arquivo
+            break;
+        }
+        
+        if (bytes_read > 0) {
+            buffer[bytes_read] = '\0';
+
+            time_t t = time(NULL);
+            struct tm *timestamp = localtime(&t);
+
+            printf("Comando recebido: %s\n", buffer);
+
+            int pid = fork();
+            if (pid == 0) {
+                // filho
+                printf("Executando comando...\n");
+
+                // divide o comando em tokens
+                char *token;
+                char **comandos = malloc(sizeof(char*) * (MAX_COMMAND_LENGTH/2 + 1));
+                int count = 0;
+                token = strtok(buffer, " ");
+                while (token != NULL) {
+                    comandos[count] = token;
+                    count++;
+                    token = strtok(NULL, " ");
+                }
+                comandos[count] = NULL;
+
+                // executa o comando
+                execvp(comandos[0], comandos);
+                perror("Erro ao executar o comando");
+
+                free(comandos);
+                exit(1);
+            }
+            else if (pid > 0) {
+                // pai
+                int status;
+                wait(&status);
+
+                time_t tf = time(NULL);
+                struct tm *timestampfinal = localtime(&tf);
+
+                printf("Comando finalizado com status %d\n", status);
+
+                char message[MAX_COMMAND_LENGTH];
+                int tamanho_str = sprintf(message, "Ended PID %d %04d-%02d-%02d %02d:%02d:%02d\n", pid,
+                    timestampfinal->tm_year + 1900, timestampfinal->tm_mon + 1, timestampfinal->tm_mday,
+                    timestampfinal->tm_hour, timestampfinal->tm_min, timestampfinal->tm_sec);
+
+                write(1, message, tamanho_str);
+            }
+            else {
+                perror("Erro ao criar processo filho");
+            }
+        }
+    }
+
+    close(canal);
+    unlink("canal");
+
+    return 0;
+}
+/*
 void armazenar_info_processo(int pid, char ** comando, struct tm *timestamp){
     int log = open("log.txt",O_WRONLY | O_CREAT | O_TRUNC,0660);
     char *buf = malloc(36 * sizeof(char));
@@ -24,5 +129,5 @@ void armazenar_info_processo(int pid, char ** comando, struct tm *timestamp){
 
 int main(){
     int comunicacao_cliente_servidor = mkfifo("comunicacao.txt");
-    
-}
+
+}*/
