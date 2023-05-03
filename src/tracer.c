ff89a3657858b0ad3188987fa72d2e8def75c06f
rafa2002 <rafacferreira2002@gmail.com>
2023-05-03 23:12:10 +0100

    Falta verificar a linha que faz strcmp(argv[1])

--- src/tracer.c
+++ src/tracer.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <string.h>
 #include <unistd.h>
 #include <wait.h>
@@ -11,88 +12,37 @@
 #include <time.h>
 #include "monitor.h"
 
-/*
-int main(int argc, char ** argv){
-
-typedef struct pedido{
-  char * pid;
-  char * nomeProg;
-  char * inicioExec  // int em ms?
-  char * fimExec   // int em ms?
-  char ** args;
-  struct pedido * prox;
-} *pedidos;
-
-//cria e adiciona pedido no início
-int addPedido (pedidos *pp, char *pid, int pr, int cmds[7],char **args){
-  pedidos p = malloc(sizeof(struct pedido));
-  p->pid = pid;
-  p->priority = pr;
-  p->args = args;
-  for (int i=0; i<7; i++) p->cmds[i]=cmds[i];
-  p->prox = (*pp);
-  (*pp) = p;
-  return 0;
-}
-
-//adiciona pedido no inicio
-int addPedido2 (pedidos *pp, pedidos p){
-  p->prox = (*pp);
-  (*pp) = p;
-  return 0;
-}
-
-// free do pedido
-void freePedido(pedidos p){
-  free(p->pid);
-  for (int i =0; p->args[i]; i++) free(p->args[i]);
-  free(p->args);
-  free(p);
-}
-
- */
-
-
 int main(int argc, char ** argv){
     
     printf("O que o utilizador mandou executar - [%s]\n",*argv);
-    
-    printf("argv[0] = %s,\n,argv[1] = %s\nargv[2] = %s",argv[0],argv[1],argv[2]);
     
     char *arg = argv[3];
     int count = 0;
     char * token;
     char **comandos = malloc(sizeof(char*)*256);
     if(arg!=NULL)
-    {
+    {   
         token = strtok(arg," ");
         while(token!=NULL){
-            comandos[count] = malloc(sizeof(char)*strlen(token)+1);
+            comandos[count] = malloc(sizeof(char *)*strlen(token)+1);
             comandos[count] = token;
             count++;
-            printf("O token é :%s",token);
+            //printf("O token é :%s\n",token);
             token = strtok(NULL," ");
-            
         }
+        comandos[count] = malloc(sizeof(NULL));
+        comandos[count] = NULL;
+        count++;
     }
-    
     int canal = open("canal", O_WRONLY, 0600);
     char * message = malloc(sizeof(int) + sizeof(char)*50 + sizeof(time_t));
-    int tamanho_str = 0;
+    int tamanho_str = 0, pid, mili_seconds_inicial = 0, mili_seconds_final = 0;
     
-    int pid;
-    int year, month, day, hour, minute, second;
-    int yearf, monthf, dayf, hourf, minutef, secondf;
-    
-    //se a operação a efetuar é o status
-    if(strcmp(argv[1], "status")==0){
-        if((pid=fork())==0){
-            
+    if(argc>=1 && strcmp(argv[1], "status")==0){
+        if((pid=fork())==0){   
             //abrir o pipe para comunicar com o servidor e pedir os programas a serem executados
-            //if ((canal = open(buffer,O_WRONLY))==-1) perror ("open error Cliente->Servidor");
-            
+            //if ((canal = open(buffer,O_WRONLY))==-1) perror ("open error Cliente->Servidor"); 
             //pede_status();
-            
             close(canal);
             _exit(0);
         }
@@ -102,62 +52,46 @@ int main(int argc, char ** argv){
         //caso não haja espaço adiciona se à lista de pedidos
         // se há recursos para realizar o comando
         if((pid=fork())==0){
+            pid = getpid();
             //Este é o filho
             //Extrai o timestamp do inicio antes da execução
-            time_t t = time(NULL);
-            struct tm *timestamp = localtime(&t);
-            
-            year = timestamp->tm_year;
-            month = timestamp->tm_mon;
-            day = timestamp->tm_mday;
-            hour = timestamp->tm_hour;
-            minute = timestamp->tm_min;
-            second = timestamp->tm_sec;
-            // Faço uma copia da string que quero escrever
-            //char* copy = malloc(sizeof(char)*tamanho_str);
-            //strcpy(copy, tamanho_str);
+            struct timeval timestamp_inicial;
+            gettimeofday(&timestamp_inicial, NULL);
+            mili_seconds_inicial = timestamp_inicial.tv_usec/1000;
             
             // Mensagem que o cliente escreve para o utilizador
-            // Mensagem format: Running PID valor_pid valor_hora
-            tamanho_str = sprintf(message,"Running %d\n", pid);
+            // Mensagem format: Running PID valor_pid valor_timestamp
+            tamanho_str = sprintf(message,"Running %d %d\n", pid, mili_seconds_inicial);
             write(1,message,tamanho_str);
             
             // Mensagem que o cliente escreve para o servidor
             // tamanho_str = sprintf(message, ... ,pid,nome_do_programa,timestamp(ms));
-
-            tamanho_str = sprintf(message,"Running %d %y-%m-%d %H:%M:%S",pid,year,month,day,hour,minute,second);
+            tamanho_str = sprintf(message,"Running %d %d",pid,mili_seconds_inicial);
             write(canal,message,tamanho_str);
-            
+            execvp(comandos[0],comandos);
+            _exit(0);
+
             //Este printf é para imprimir no stdout o comando a executar informando assim o user
             printf("PID[%d] para executar: ",pid);
-            for(int c = 1; c<strlen( comandos);c++){
+            for(int c = 1; c<strlen(*comandos);c++){
                 printf("%s ",comandos[c]);
                 printf("\n");
-            }
-            
-            execvp(comandos[0],comandos);
-            
+            }        
         }
-        
         else{
             //Este é o pai
-            
+            printf("passou aqui --- obstaculo pai\n");
             // Extrai o timestamp do fim após a execução
-            time_t tf = time(NULL);
-            struct tm *timestampfinal = localtime(&tf);
-            yearf = timestampfinal->tm_year;
-            monthf = timestampfinal->tm_mon;
-            dayf = timestampfinal->tm_mday;
-            hourf = timestampfinal->tm_hour;
-            minutef = timestampfinal->tm_min;
-            secondf = timestampfinal->tm_sec;
+            struct timeval timestamp_final;
+            gettimeofday(&timestamp_final, NULL);
+            mili_seconds_final = timestamp_final.tv_usec/1000;
+
             // Mensagem que o cliente escreve para o servidor a avisar que concluiu
-            // tamanho_str = sprintf(message, ... ,pid,nome_do_programa,timestamp(ms));
-            //tamanho_str = sprintf(message,"%d %Y-%m-%d %H:%M:%S",pid,yearf,monthf,dayf,hourf,minutef,secondf);
-            tamanho_str = sprintf(message,"%d %y-%m-%d %H:%M:%S",pid,yearf,monthf,dayf,hourf,minutef,secondf);
+            tamanho_str = sprintf(message,"End %d %d",pid,mili_seconds_final);
 
             write(canal,message,tamanho_str);
-            while(count>=0){
+            
+            while(count>0){
                 free(comandos[count]);
                 count--;
             }
@@ -165,9 +99,6 @@ int main(int argc, char ** argv){
             wait(NULL);
         }
     }
-
-
     // caso n haja mais pedidos fecha servidor
-    
     return 0;
 }
